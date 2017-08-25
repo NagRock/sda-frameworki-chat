70160e41c94d20360cb38f8c9ef68f2ce0681bb8
mkuster <kuster.maciej@gmail.com>
2017-08-25 19:32:00 +0200

    Added e2e spec checking if login button is disabled when login input field is empty

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,2 +1,22 @@
+import {browser, by, element} from 'protractor';
+
 describe('chat App', () => {
+  beforeEach(() => {
+    browser.ignoreSynchronization = true;
+    browser.get('/');
+  });
+
+  it('should display login label', () => {
+    const loginLabel = element(by.tagName('h3'));
+    expect(loginLabel.getText()).toEqual('Login:');
+  });
+
+  describe('login button', () => {
+    it('should be disabled when login input field is empty', () => {
+      const inputField = element(by.tagName('input'));
+      inputField.clear();
+      const loginButton = element(by.tagName('button'));
+      expect(loginButton.isEnabled()).toBeFalsy();
+    });
+  });
 });
